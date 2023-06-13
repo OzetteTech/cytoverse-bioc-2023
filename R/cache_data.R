e4dbdc21989431e3b8ea023f0ad6fad35ed10a55
Andrew McDavid <andrew@ozette.com>
2023-06-13 16:01:36 -0700

    add path argument

--- R/cache_data.R
+++ R/cache_data.R
@@ -1,7 +1,7 @@
 bfc = BiocFileCache::BiocFileCache(ask = FALSE)
 zip_link = "https://figshare.com/ndownloader/files/41233761?private_link=5d52dfbf8481d1cfffc8"
 
-#' Download and cache workshop data
+#' Download and cache workshop data from CytoverseBioc2023:::zip_link
 #' @param force Should we download and cache again even if it appears that data have already been downloaded?
 #' @return [BiocFileCache::BiocFileCache()], invisibly
 #' @export
@@ -23,6 +23,7 @@ cache_workshop_data = function(force = FALSE){
 
 #' @export
 #' @describeIn cache_workshop_data return a `data.frame` with `rpath` pointing to the location of files whose paths match `path`
+#' @param path `character` giving a path to file(s) in the original tarball
 get_workshop_data = function(path){
   bfcquery(bfc, path)
 }
